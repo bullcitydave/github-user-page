7039d7178cb1788d72cd9a9b9965f235b84106fa
Dave Seidman <bullcitydave@gmail.com>
2014-06-21 16:37:37 -0400

    Error with repo view--main section--rendering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,16 @@
-		// When rending an underscore template, we want top-level
-		// variables to be referenced as part of an object. For
-		// technical reasons (scope-chain search), this speeds up
-		// rendering; however, more importantly, this also allows our
-		// templates to look / feel more like our server-side
-		// templates that use the rc (Request Context / Colletion) in
-		// // order to render their markup.
-
-    // _.templateSettings.variable = 'rc';
-
-		// Grab the HTML out of our template tag and pre-compile it.
-    // ??  this is from the other example Julia sent me
-		// var template = _.template(
-		// 	$( 'script.user-template' ).html()
-		// );
-
-    var userView = $('.user-template').html();
-
-		// Define our render data (to be put into the 'rc' variable).
-
-		var templateData = {
-			name: 'Dave Seidman',
-      login: 'bullcitydave',
-      email: 'bullcitydave@gmail.com',
-      blog: 'dave.seidman@me.com',
-      location: 'Durham, NC',
-      created_at: 'June 4, 2014',
-      following: 6,
-      followers: 12,
-      avatar_url: 'https://avatars.githubusercontent.com/u/7648787?',
-      organizations_url: 'https://api.github.com/users/bullcitydave/orgs'
-    	};
+     // To test out template before using JSON file
+		// var templateData = {
+		// 	name: 'Dave Seidman',
+    //   login: 'bullcitydave',
+    //   email: 'bullcitydave@gmail.com',
+    //   blog: 'dave.seidman@me.com',
+    //   location: 'Durham, NC',
+    //   created_at: 'June 4, 2014',
+    //   following: 6,
+    //   followers: 12,
+    //   avatar_url: 'https://avatars.githubusercontent.com/u/7648787?',
+    //   organizations_url: 'https://api.github.com/users/bullcitydave/orgs'
+    // 	};
 
 
 
@@ -597,5 +578,28 @@ var repoJSON =
 // $( '.template' ).after(
 // 	template(templateData)
 // );
+// When rending an underscore template, we want top-level
+// variables to be referenced as part of an object. For
+// technical reasons (scope-chain search), this speeds up
+// rendering; however, more importantly, this also allows our
+// templates to look / feel more like our server-side
+// templates that use the rc (Request Context / Colletion) in
+// // order to render their markup.
 
-  $('header').append(_.template(userView,userJSON));
+// _.templateSettings.variable = 'rc';
+
+// Grab the HTML out of our template tag and pre-compile it.
+// ??  this is from the other example Julia sent me
+// var template = _.template(
+// 	$( 'script.user-template' ).html()
+// );
+
+var userView = $('.user-template').html();
+
+var repoView = $('.repo-template').html();
+
+// Define our render data (to be put into the 'rc' variable).
+
+$('#user-info').append(_.template(userView,userJSON));
+
+$('#main').append(_.template(repoView,repoJSON));
